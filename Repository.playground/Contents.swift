c6a375c3d006693543aeab5ccc5c837a884b75a2
Student Account <student@mac-prod1.local>
2019-10-03 14:28:46 -0700

    crud methods

--- Repository.playground/Contents.swift
+++ Repository.playground/Contents.swift
@@ -18,6 +18,46 @@ class UserRepository {
             task.resume()
         }
     }
+    
+    func create(withUser user: User, withCompletion completion: @escaping (User?) -> Void) {
+        let URLString = path
+        var postRequest = URLRequest.init(url: URL.init(string: URLString)!)
+        postRequest.httpMethod = "POST"
+        postRequest.httpBody = try? JSONEncoder().encode(user)
+        
+        let task = URLSession.shared.dataTask(with: postRequest, completionHandler: {
+            (data, response, error) in
+            if let user = try? JSONDecoder().decode(User.self, from: data!) {
+                completion (user)
+            }
+        })
+        task.resume()
+    }
+    
+    func update(withId id:Int, withUser user:User) {
+        let URLString = path + "\(id)"
+        var updateRequest = URLRequest.init(url: URL.init(string: URLString)!)
+        updateRequest.httpMethod = "PUT"
+        updateRequest.httpBody = try? JSONEncoder().encode(user)
+        
+        let task = URLSession.shared.dataTask(with: updateRequest) {
+            (data, response, error) in
+            print(String.init(data: data!, encoding: .ascii) ?? "Error: \(error!)")
+        }
+        task.resume()
+    }
+    
+    func delete(withId id:Int) {
+        let URLString = path + "\(id)"
+        var deleteRequest = URLRequest.init(url: URL.init(string: URLString)!)
+        deleteRequest.httpMethod = "DELETE"
+
+        let task = URLSession.shared.dataTask(with: deleteRequest){
+            (data, response, error) in
+            print("Delete Successful")
+        }
+        task.resume()
+    }
 }
 
 class User: Codable {
@@ -32,8 +72,23 @@ class User: Codable {
 let userRepo = UserRepository(withPath: "http://216.186.69.45/services/device/users/")
 
 //Fetch a single User
-userRepo.fetch(withId: 43, withCompletion: {(user) in
+userRepo.fetch(withId: 58, withCompletion: {(user) in
         print(user!.FirstName ?? "no user")
 })
 
+// Create (post) new user
+var mUser = User()
+mUser.FirstName = "jim"
+mUser.LastName = "beam"
+userRepo.create(withUser: mUser, withCompletion: {(user) in
+    print("new user: \(user!.FirstName ?? "no user") \(user!.LastName ?? "no user")")
+})
 
+// Update (put) user
+var nUser = User()
+nUser.FirstName = "the"
+nUser.LastName = "glenlivet"
+userRepo.update(withId: 58, withUser: nUser)
+
+// Delete user
+userRepo.delete(withId: 11)
