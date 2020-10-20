368d4df2aa182a2ed9fa2e2f5d417bf9eedcccfb
Charles Sy <charles.m.sy@hotmail.com>
2020-10-20 15:28:22 -0700

    Updated

--- Repository.playground/Contents.swift
+++ Repository.playground/Contents.swift
@@ -1,29 +1,35 @@
 import UIKit
 
-class MusicRepository {
+protocol hasID {
+    var id: Int?{get set}
+}
+
+class MusicRepository <genericObject> where genericObject : Codable, genericObject: hasID {
     var path: String
     init(withPath path:String){
         self.path = path
     }
     
+    
     // READ a single object------
-    func fetch(withId id: Int, withCompletion completion: @escaping (Music?) -> Void) {
-        let URLstring = path + "music/id/\(id)"
+    func fetch(withId id: Int, withCompletion completion: @escaping (genericObject?) -> Void) {
+        let URLstring = path + "id/\(id)"
         if let url = URL.init(string: URLstring){
             let task = URLSession.shared.dataTask(with: url, completionHandler:
             {(data, response, error) in
-                if let user = try? JSONDecoder().decode(Music.self, from: data!){
-                    completion (user)
+                if let music = try? JSONDecoder().decode(genericObject.self, from: data!){
+                    completion (music)
                 }
             })
             task.resume()
+            
         }
     }
     
     //TODO: Build and test comparable methods for the other CRUD items
-    func create(a:Music) {
+    func create(a:genericObject) {
         guard a.id != nil else {return}
-        let URLstring = path + "music/id/\(a.id!)"
+        let URLstring = path + "id/\(a.id!)"
         var postRequest = URLRequest.init(url: URL.init(string: URLstring)!)
         
         postRequest.httpMethod = "POST"
@@ -36,9 +42,9 @@ class MusicRepository {
         task.resume()
     }
     
-    func update( withId id:Int, a:Music) {
+    func update( withId id:Int, a:genericObject) {
         guard a.id != nil else {return}
-        let URLstring = path + "music/id/\(a.id!)"
+        let URLstring = path + "id/\(a.id!)"
         var putRequest = URLRequest.init(url: URL.init(string: URLstring)!)
         
         putRequest.httpMethod = "PUT"
@@ -52,7 +58,7 @@ class MusicRepository {
     }
     
     func delete( withId id:Int ) {
-         let URLstring = path + "music/id/\(id)"
+         let URLstring = path + "id/\(id)"
          var deleteRequest = URLRequest.init(url: URL.init(string: URLstring)!)
          
          deleteRequest.httpMethod = "DELETE"
@@ -65,23 +71,23 @@ class MusicRepository {
     
 }
 
-class Music: Codable {
-    var id: String?
+class Music: Codable, hasID{
+    var id: Int?
     var music_url: String?
     var name: String?
     var description: String?
 }
 
 //Create a User Repository for the API at http://216.186.69.45/services/device/users/
-let musicRepo = MusicRepository(withPath: "https://www.orangevalleycaa.org/api/")
+let musicRepo = MusicRepository<Music>(withPath: "https://www.orangevalleycaa.org/api/music/")
 
 //Fetch a single Music
-musicRepo.fetch(withId: 10, withCompletion: {(music) in
-   print(music!.name ?? "no music")
+musicRepo.fetch(withId: 10, withCompletion: {(music)
+    in print(music!.name ?? "no music")
 })
 
 let addMusic = Music()
-addMusic.id = "11"
+addMusic.id = 11
 addMusic.music_url = "www.google.com"
 addMusic.name = "Test Title"
 addMusic.description = "Test Description"
@@ -91,7 +97,7 @@ musicRepo.create(a: addMusic)
 
 
 let updateMusic = Music()
-updateMusic.id = "1"
+updateMusic.id = 1
 updateMusic.music_url = "www.google.com"
 updateMusic.name = "Test2 Title"
 updateMusic.description = "Test2 Description"
@@ -101,32 +107,3 @@ musicRepo.update(withId: 1, a: updateMusic)
 
 //Delete a single Music
 musicRepo.delete(withId: 1)
-
-/**
-// * TODO: // Refactor the code using Generics and protocols so that you can re-use it as shown below
-// *
-// //Create a User Repository for the API at http://216.186.69.45/services/device/users/
-// let userRepo = Repository<User>(withPath: "http://216.186.69.45/services/device/users/")
-//
-// //Fetch a single User
-// userRepo.fetch(withId: 43, withCompletion: {(user) in
-//    print(user!.FirstName ?? "no user")
-// })
-//
-// // Another type of object
-// class Match: Codable {
-// var name: String?
-// var password: String?
-// var countTIme: String?
-// var seekTime: String?
-// var status: String?
-// }
-// //Create a Match Repository for a different API at http://216.186.69.45/services/hidenseek/matches/
-// let matchRepo = Repository<Match>(withPath: "http://216.186.69.45/services/hidenseek/matches/")
-//
-// //Fetch a single User
-// matchRepo.fetch(withId: 1185, withCompletion: {(match) in
-//    print(match!.status ?? "no match")
-// })
-//
- **/
