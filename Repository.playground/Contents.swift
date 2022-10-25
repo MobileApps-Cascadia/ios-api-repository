a221c207f4af3134bace4ec4f4de88592c8cfe3b
asb43 <pokemonfanboy600@gmail.com>
2022-10-24 17:51:45 -0700

    completed?

--- Repository.playground/Contents.swift
+++ Repository.playground/Contents.swift
@@ -1,12 +1,12 @@
 import UIKit
 
-class UserRepository {
+class UserRepository<Element: Codable> {
     var path: String
     init(withPath path:String){
         self.path = path
     }
     // READ a single object
-    func fetch(withId id: Int, withCompletion completion: @escaping (User?) -> Void) {
+    func fetch(withId id: Int, withCompletion completion: @escaping (Element?) -> Void) {
         let URLstring = path + "\(id)"
         if let url = URL.init(string: URLstring){
             let task = URLSession.shared.dataTask(with: url, completionHandler:
@@ -15,7 +15,7 @@ class UserRepository {
                 let str = String(decoding: data!, as: UTF8.self)
                 print("Responding to request data: " + str)
                 
-                if let user = try? JSONDecoder().decode(User.self, from: data!){
+                if let user = try? JSONDecoder().decode(Element.self, from: data!){
                     print("Running completion closure")
                     completion (user)
                 }
@@ -23,42 +23,44 @@ class UserRepository {
             task.resume()
         }
     }
+
+    func create( a:User , withCompletion completion: @escaping (String?) -> Void) {
+        let URLstring = path + "/id/\(a.UserID!)"
+        
+        var postRequest = URLRequest.init(url: URL.init(string: URLstring)!)
+        
+        postRequest.httpMethod = "POST"
+        postRequest.httpBody = try? JSONEncoder().encode(a)
+       //postRequest.setValue("application/json", value(forHTTPHeaderField: Content-Type))
+        let task = URLSession.shared.dataTask(with: postRequest) {
+            (data, response, error) in
+                print (String.init(data: data!, encoding: .ascii) ?? "no data")
+        }
+
+        task.resume()
+    }
     
-    //TODO: Build and test comparable methods for the other CRUD items
-    func create( a:User , withCompletion completion: @escaping (User?) -> Void) {
-        guard a.UserID != nil else {return}
-        let URLstring = path + "id/\(a.UserID!)"
-        var post = URLRequest.init(url: URL.init(string: URLstring)!)
-        
-        post.httpMethod = "POST"
-        post.httpBody = try? JSONEncoder().encode(a)
-        let task = URLSession.shared.dataTask(with: post) {(data, response, error) in
-                    print (String.init(data: data!, encoding: .ascii) ?? "no data added")
-    }
-        task.resume()
-    }
     func update( withId id:Int, a:User) {
-        guard a.UserID != nil else {return}
-        let URLstring = path + "id/\(a.UserID!)"
-        var put = URLRequest.init(url: URL.init(string: URLstring)!)
-        
-        put.httpMethod = "PUT"
-        put.httpBody = try? JSONEncoder().encode(a)
-        let task = URLSession.shared.dataTask(with: put) {(data, response, error) in
-                    print (String.init(data: data!, encoding: .ascii) ?? "no data updated")
+        let urlString = path + "/id/\(a.UserID!)"
+        var updateRequest = URLRequest.init(url: URL.init(string: urlString)!)
+        updateRequest.httpMethod = "PUT"
+        updateRequest.httpBody = try? JSONEncoder().encode(a.self)
+        URLSession.shared.dataTask(with: updateRequest) {
+            (data, response, error) in
+                print (String.init(data: data!, encoding: .ascii) ?? "no data")
+        }
     }
-        task.resume()
-    }
+    
     func delete( withId id:Int ) {
-        let URLstring = path + "id/\(id)"
-        var delete = URLRequest.init(url:URL.init(string: URLstring)!)
-        delete.httpMethod = "DELETE"
-        let task = URLSession.shared.dataTask(with: delete) {(data,response,error) in
-            print (String.init(data: data!, encoding: .ascii) ?? "no data deleted")
+        let urlString = path + "/id/\(id)"
+        var deleteRequest = URLRequest.init(url: URL.init(string: urlString)!)
+        deleteRequest.httpMethod = "DELETE"
+        let task = URLSession.shared.dataTask(with: deleteRequest) {
+            (data, response, error) in
+                print (String.init(data: data!, encoding: .ascii) ?? "no data")
         }
         task.resume()
     }
-    
 }
 
 class User: Codable {
@@ -70,21 +72,14 @@ class User: Codable {
 }
 
 //Create a User Repository for the API at https://mikethetall.pythonanywhere.com/users
-let userRepo = UserRepository(withPath: "https://mikethetall.pythonanywhere.com/users/")
+let userRepo = UserRepository<User>(withPath: "https://mikethetall.pythonanywhere.com/users/")
 
 print("About start fetch")
 //Fetch a single User
 userRepo.fetch(withId: 1, withCompletion: {(user) in
         print(user!.FirstName ?? "no user")
 })
-
 print("Done initiating fetch")
-
-/*
- * TODO: // Refactor the code using Generics and protocols so that you can re-use it as shown below
- *
- //Create a User Repository for the API at https://mikethetall.pythonanywhere.com/users/users/
- let userRepo = Repository<User>(withPath: "https://mikethetall.pythonanywhere.com/users/users/")
  
  //Fetch a single User
  userRepo.fetch(withId: 2, withCompletion: {(user) in
@@ -99,11 +94,3 @@ print("Done initiating fetch")
  var seekTime: String?
  var status: String?
  }
- //Create a Match Repository for a different API at http://216.186.69.45/services/hidenseek/matches/
- let matchRepo = Repository<Match>(withPath: "http://216.186.69.45/services/hidenseek/matches/")
- 
- //Fetch a single User
- matchRepo.fetch(withId: 1185, withCompletion: {(match) in
-    print(match!.status ?? "no match")
- })
-*/
